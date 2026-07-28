a006010a9b53846e09efb99d3f5fbc73c701dfb0
Ashutoshporwal-ashu <ashutoshporwal77@gmail.com>
2026-07-28 19:01:45 +0530

    feat: add API error and response handling classes, and update dependencies

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (requesrHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requesrHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
