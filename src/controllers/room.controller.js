bb6a47ace9e527696fb65d28a2eaec4ae32a9014
Ashutoshporwal-ashu <ashutoshporwal77@gmail.com>
2026-08-05 16:22:58 +0530

    feat: add room detail retrieval and code saving functionality for private and public rooms

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -41,7 +41,124 @@ const CreatePublicRoom = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, PublicRoom, "Public room is created"))
 })
 
+
+const getPublicRoom = asyncHandler(async (req, res) => {
+    
+    const {roomId} = req.params
+
+    if(!roomId){
+        throw new ApiError(400, "Public room Id is required")
+    }
+    
+    const PublicRoomDetails = await Room.findOne({
+        roomId: roomId,
+        roomType: "public"
+    })
+
+    if(!PublicRoomDetails){
+        throw new ApiError(404, "Public room is not exist with this room id")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, PublicRoomDetails, "public room details fatched successfull"))
+})
+
+const getPrivateRoom = asyncHandler(async (req, res) => {
+    
+    const {roomId} = req.params
+
+    if(!roomId){
+        throw new ApiResponse(400, "Private room Id is required")
+    }
+
+    const PrivateRoomDetails = await Room.findOne({
+        roomId: roomId,
+        roomType: "private"
+    }).populate("host", "fullname email")
+
+    if(!PrivateRoomDetails){
+        throw new ApiError(404, "Private Room is not exist with this room Id")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, PrivateRoomDetails, "Private Room Details fatched successfully"))
+})
+
+const savePublicRoomCode = asyncHandler(async (req, res) => {
+    const {roomId} = req.params
+    const {code, language} = req.body
+
+    if(!roomId){
+        throw new ApiError(400, "Public room Id is required for save this code")
+    }
+
+    const updatedPublicRoom = await Room.findOneAndUpdate(
+        {
+            roomId: roomId,
+            roomType: "public"
+        },
+        {
+            $set: {
+                code: code,
+                language: language
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updatedPublicRoom){
+        throw new ApiError(404, "Public room not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "code saved successfully"))
+})
+
+const savePrivateRoomCode = asyncHandler(async (req, res) => {
+    const { roomId } = req.params
+    const {code, language} = req.body
+    const userId = req.user._id
+
+    if(!roomId){
+        throw new ApiError(400, "Private room id is required")
+    }
+
+    const updatedPrivateRoomCode = await Room.findOneAndUpdate(
+        {
+            roomId: roomId,
+            roomType: "private"
+        },
+        {
+            $set: {
+                code: code,
+                language: language
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updatedPrivateRoomCode){
+        throw new ApiError(404, "Private room is not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Private Room updated successfully"))
+})
+
+
 export {
     CreatePrivateRoom,
-    CreatePublicRoom
+    CreatePublicRoom,
+    getPrivateRoom,
+    getPublicRoom,
+    savePrivateRoomCode,
+    savePublicRoomCode
 }
--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,10 +1,20 @@
-import { CreatePrivateRoom, CreatePublicRoom } from "../controllers/room.controller";
-import { verifyJWT } from "../middlewares/auth.middleware";
+import { rootCertificates } from "tls";
+import { CreatePrivateRoom, CreatePublicRoom, getPrivateRoom, getPublicRoom, savePrivateRoomCode, savePublicRoomCode } from "../controllers/room.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 
 const router = Router()
 
 router.route("/create-private-room").post(verifyJWT, CreatePrivateRoom)
 router.route("/create-public-room").post(CreatePublicRoom)
+
+// get details of room
+router.route("/private/:roomId")
+    .get(verifyJWT, getPrivateRoom)
+    .put(verifyJWT, savePrivateRoomCode) // save code in private room
+
+router.route("/public/:roomId")
+    .get(getPublicRoom)
+    .put(savePublicRoomCode) // save code in public room
 
 export default router
