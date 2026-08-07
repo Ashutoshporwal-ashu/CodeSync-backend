fb21dc8cfecc83edd1a8336a721ac5ed2b5083ab
Ashutoshporwal-ashu <ashutoshporwal77@gmail.com>
2026-08-07 22:27:32 +0530

    feat: integrate socket.io for real-time communication

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
+import http from "http"
+import { Server } from  "socket.io"
+
 dotenv.config({
     path: './env'
 })
 
+const httpServer = http.createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (Socket) => {
+    console.log(`New User is connected: ${Socket.id}`)
+
+    Socket.on(disconnect, () => {
+        console.log(`User Disconnected: ${socket.id}`)
+    })
+})
+
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    httpServer.listen(process.env.PORT || 8000, () => {
         console.log(`server is running at port: ${process.env.PORT}`)
     })
 })
